750b7a56edbc42b3c3b4bebe149a324454a9155e
Adam Glowicki <adamglowicki@wp.pl>
2018-06-26 09:23:30 +0200

    Dodałem kilka klas ale musze zie dobrze zastanowic i nzalezc ich wspolne cechy

---
+++ src/Menu.java
@@ -0,0 +1,14 @@
+public class Menu {
+
+    private OurForecast ourForecast = new OurForecast();
+    private Calculator calculator = new Calculator();
+    private Credit credit = new Credit();
+
+    public Menu() {
+        this.menu();
+    }
+
+    private void menu(){
+
+    }
+}
