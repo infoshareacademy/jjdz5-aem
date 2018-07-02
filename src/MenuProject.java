48fe2fd190b7420f27cbf2cd4ccf5d1d196c34bb
Elka165 <ela_dolna99@o2.pl>
2018-07-02 16:34:23 +0200

    Usunięcie zbędnych komentarzy

--- src/MenuProject.java
+++ src/MenuProject.java
@@ -60,24 +60,4 @@ public class MenuProject {
 
 }
 
-//    public void Currency() {
-//
-//
-//        String[] tablica={"PLN", "WA", "EU"};
-//        Set<String> current=new HashSet<>();
-//        for(int i=0; i<tablica.length; i++) {
-//            current.add(tablica[i]);
-//        }
-//
-//        Map<Integer,String> mapa=new HashMap<Integer, String>();
-//        int i=1;
-//        System.out.println("Program posiada poniższe waluty: ");
-//        for(String arg:current) {
-//            mapa.put(i,arg);
-//            System.out.println(i + "." + arg );
-//            i++;
-//        }
-//
-//
-// }
-//}
+
