5e8aa3976e1a1d557ae4ecceb0a143686e87d94e
Elka165 <ela_dolna99@o2.pl>
2018-07-08 22:28:31 +0200

    Dodanie klasy CurrentCurrency

---
+++ src/CurrentCurrency.java
@@ -0,0 +1,65 @@
+
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+
+public class CurrentCurrency {
+    LocalDate localTime1 = LocalDate.now() ;
+    String dateCurrent;
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CurrentCurrency that = (CurrentCurrency) o;
+        return Objects.equals(localTime1, that.localTime1) &&
+                Objects.equals(dateCurrent, that.dateCurrent);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(localTime1, dateCurrent);
+    }
+
+
+    public void currentCash() {
+     FileContentReader fileContentReader = new FileContentReader();
+     fileContentReader.readFile();
+     Map<String,Double> map=new HashMap<>();
+        checkDate();
+
+        System.out.println(dateCurrent);
+     for (Currency currency : fileContentReader.getListOfCurrencies()) {
+         if (currency.getDate().toString().equals(dateCurrent)) {
+             map.put(currency.getName(), currency.getClose());
+
+         }
+
+     }
+        System.out.println(map.toString());
+
+
+ }
+
+ public String checkDate(){
+
+        switch (localTime1.getDayOfWeek()){
+            case SUNDAY: dateCurrent=localTime1.minusDays(2).format(DateTimeFormatter.ISO_LOCAL_DATE);
+            break;
+            case SATURDAY:dateCurrent=localTime1.minusDays(1).format(DateTimeFormatter.ISO_LOCAL_DATE);
+            break;
+            default: dateCurrent=localTime1.format(DateTimeFormatter.ISO_LOCAL_DATE);
+            break;
+
+
+        }
+
+        return dateCurrent;
+ }
+
+}
