5133ec23df6c19f1fed6302a2ae5beb2d48db1a4
Elka165 <ela_dolna99@o2.pl>
2018-07-15 18:19:53 +0200

    Obsługa Stringu w MenuProject

--- src/MenuProject.java
+++ src/MenuProject.java
@@ -2,6 +2,7 @@
 
 import javax.swing.event.SwingPropertyChangeSupport;
 import java.util.*;
+import java.util.regex.MatchResult;
 
 public class MenuProject {
 
@@ -16,45 +17,54 @@ MenuInformation menuInformation=new MenuInformation();
 
 
             System.out.println("Wybierz interesującą Cię metodę");
-            int choose=0;
+
+           // int choose=0;
 
             do{
                 Scanner scanner = new Scanner(System.in);
-                choose=scanner.nextInt();
 
-                switch (choose) {
-                    case 1:
-                     menuInformation.programInformation();
-                     break;
-                    case 2:
-                        System.out.println("Metoda umożliwia wczytanie pliku do konsoli");
-                        break;
-                    case 3:
-                        menuInformation.currentCurrency();
-                        break;
-                    case 4:
-                        System.out.println("Metoda pokazuje globalną wartość min oraz max walut");
-                        break;
-                    case 5:
-                        System.out.println("Metoda pokazuje lokalną wartość min oraz max  walut na podstawie wybranego zakresu czasowego");
-                        break;
-                    case 6:
-                        System.out.println("Metoda umożliwia przeliczenia wskazanej waluty");
-                        break;
-                    case 7:
-                        menuPanel();
-                        break;
-                    case 0:
-                        System.out.println("Dziękujemy za skorzystanie z programu\n");
-                        break;
-                     default:
-                         System.out.println("MenuProject nie posiada numeru " + choose + ". Podaj ponownie numer metody, którą chcesz wywołać");
-                         break;
-                }
-            } while(choose!=0);
+              String  choose=scanner.nextLine();
 
-            System.out.println("     ****************************************");
-            System.out.println("     Koniec programu\n\n");
+                if(choose.matches("\\d{0,9}")) {
+                    Integer choose1=Integer.parseInt(choose);
+                    switch (choose1) {
+                        case 1:
+                            menuInformation.programInformation();
+                            break;
+                        case 2:
+                            System.out.println("Metoda umożliwia wczytanie pliku do konsoli");
+                            break;
+                        case 3:
+                            menuInformation.currentCurrency();
+                            break;
+                        case 4:
+                            System.out.println("Metoda pokazuje globalną wartość min oraz max walut");
+                            break;
+                        case 5:
+                            System.out.println("Metoda pokazuje lokalną wartość min oraz max  walut na podstawie wybranego zakresu czasowego");
+                            break;
+                        case 6:
+                            System.out.println("Metoda umożliwia przeliczenia wskazanej waluty");
+                            break;
+                        case 7:
+                            menuPanel();
+                            break;
+                        case 0:
+                            System.out.println("Dziękujemy za skorzystanie z programu\n");
+                            System.out.println("     ****************************************");
+                            System.out.println("     Koniec programu\n\n");
+                            System.exit(0);
+
+                        default:
+                            System.out.println("MenuProject nie posiada numeru " + choose + ". Podaj ponownie numer metody, którą chcesz wywołać");
+                            break;
+                    }
+
+                    }else {
+                    System.out.println("MenuProject nie posiada numeru " + choose + ". Podaj ponownie numer metody, którą chcesz wywołać");
+                }} while (true);
+
+
         }
 
 }
