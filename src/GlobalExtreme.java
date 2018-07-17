652add5ff7398202776f2619eec71ef7266b0a10
Adam Glowicki <adamglowicki@wp.pl>
2018-07-17 10:44:13 +0200

    preparation for the global extreme

--- src/GlobalExtreme.java
+++ src/GlobalExtreme.java
@@ -1,26 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
-
 public class GlobalExtreme {
 
-    private FileContentReader fileContentReader = new FileContentReader();
-    private GlobalExtremeMenu globalExtremeMenu = new GlobalExtremeMenu();
-    private Currency currency;
-
-    public void run() {
-        findCurrency();
-    }
-
-    private void findCurrency() {
-        List<Currency> singleCurrency = new ArrayList<>();
-        for (Currency c:fileContentReader.getCurrencies()) {
-            if (c.getName().equals(globalExtremeMenu.getChooseCommand())){
-                singleCurrency.add(c);
-            }
-        }
-        for (Currency c: singleCurrency) {
-            System.out.println(c);
-        }
-    }
 
 }
--- src/GlobalExtremeMenu.java
+++ src/GlobalExtremeMenu.java
@@ -1,45 +1,17 @@
-import java.util.Arrays;
-import java.util.List;
-
 public class GlobalExtremeMenu {
 
     private final ConsolePrinter consolePrinter = new ConsolePrinter();
     private final ConsoleReader consoleReader = new ConsoleReader();
-    private final IgnoreCase ignoreCase = new IgnoreCase();
-    private ListAvailableCurrency listAvailableCurrency = new ListAvailableCurrency();
-    private FeatureReader featureReader = new FeatureReader();
 
-    private final String currencies = "currencies";
-    private final String extreme = "extreme";
-    private String chooseCommand;
-
-    public void run() {
-        printOptjons();
-        menu();
-    }
-
-    public String getChooseCommand() {
-        return chooseCommand;
-    }
 
     private void printOptjons() {
         consolePrinter.printLn("Choose what do you want to do");
-        consolePrinter.printLn("If you want print available currencies enter: \"currencies\"");
-        consolePrinter.printLn("If you want find extreme enter: \"extreme\"");
+        consolePrinter.printLn("Choose what do you want to do");
     }
 
     public void menu() {
-        this.chooseCommand = consoleReader.getString("Choose command");
-        ignoreCase.lowerSize(chooseCommand);
-        featureReader.predictedLiteray(chooseCommand);
+        String chooseCommand = consoleReader.getString("Choose command");
         switch (chooseCommand) {
-            case "currencies":
-                listAvailableCurrency.run();
-                break;
-            case "extreme":
-
-                break;
-
 
         }
     }
