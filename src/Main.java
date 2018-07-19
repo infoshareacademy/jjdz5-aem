8cc88ca2fe6c8e860d19409803b00ab60e36802b
Adam Glowicki <adamglowicki@wp.pl>
2018-07-19 07:16:36 +0200

    add a literaty readking feautre

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        FileContentReader fileContentReader = new FileContentReader();
-        CurrencyRepository currencyRepository = new CurrencyRepository();
-        Currency currency = new Currency();
-        fileContentReader.readFile();
-        System.out.println(currencyRepository.find());
-        currencyRepository.findName();
+        IgnoreCase ignoreCase =new IgnoreCase();
+        ignoreCase.predictedLiteray("waldamar");
+        ignoreCase.print();
+
 
     }
 }
