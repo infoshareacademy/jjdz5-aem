45bb2c94f7ce2d8b132288a611b9dcc31c93f927
Adam Glowicki <adamglowicki@wp.pl>
2018-07-19 07:16:36 +0200

    add a literaty readking feautre

--- src/GlobalExtremeMenu.java
+++ src/GlobalExtremeMenu.java
@@ -1,17 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class GlobalExtremeMenu {
 
     private final ConsolePrinter consolePrinter = new ConsolePrinter();
     private final ConsoleReader consoleReader = new ConsoleReader();
+    private final IgnoreCase ignoreCase = new IgnoreCase();
+
+    private final String currencies = "currencies";
+    private final String extreme = "extreme";
 
 
     private void printOptjons() {
         consolePrinter.printLn("Choose what do you want to do");
-        consolePrinter.printLn("Choose what do you want to do");
+        consolePrinter.printLn("If you want print available currencies enter: \"currencies\"");
+        consolePrinter.printLn("If you want find extreme enter: \"extreme\"");
     }
 
     public void menu() {
         String chooseCommand = consoleReader.getString("Choose command");
+        ignoreCase.ignoreSize(chooseCommand);
+        ignoreCase.predictedLiteray(chooseCommand);
         switch (chooseCommand) {
+            case "currencies":
+                break;
+            case "extreme":
+                break;
+
 
         }
     }
