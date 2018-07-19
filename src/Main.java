c9a7b5431f0298e3814d1f4817f32430477ba2b7
Adam Glowicki <adamglowicki@wp.pl>
2018-07-19 07:16:36 +0200

    add a literaty readking feautre

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        FileContentReader fileContentReader = new FileContentReader();
-        fileContentReader.readFile();
-        GlobalExtremeMenu globalExtremeMenu = new GlobalExtremeMenu();
-        globalExtremeMenu.run();
+        IgnoreCase ignoreCase =new IgnoreCase();
+        ignoreCase.predictedLiteray("waldamar");
+        ignoreCase.print();
+
+
     }
 }
