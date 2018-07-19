db7d0a3200fae223a30b2a25107ffe9002b1e0a5
Adam Glowicki <adamglowicki@wp.pl>
2018-07-19 09:37:28 +0200

    List currency done

--- src/GlobalExtreme.java
+++ src/GlobalExtreme.java
@@ -4,23 +4,17 @@ import java.util.List;
 public class GlobalExtreme {
 
     private FileContentReader fileContentReader = new FileContentReader();
-    private ListAvailableCurrency listAvailableCurrency = new ListAvailableCurrency();
-    private ConsoleReader consoleReader = new ConsoleReader();
-    private IgnoreCase ignoreCase = new IgnoreCase();
+    private GlobalExtremeMenu globalExtremeMenu = new GlobalExtremeMenu();
+    private Currency currency;
 
     public void run() {
         findCurrency();
     }
 
-
-
     private void findCurrency() {
-        String availableCurrency = consoleReader.getString("Enter available currency");
-        String s = ignoreCase.upperSize(availableCurrency);
-        System.out.println(s);
         List<Currency> singleCurrency = new ArrayList<>();
         for (Currency c:fileContentReader.getCurrencies()) {
-            if (c.getName().equals(s)){
+            if (c.getName().equals(globalExtremeMenu.getChooseCommand())){
                 singleCurrency.add(c);
             }
         }
