809d9a32f7e2063a6a5726f52443a808c34fd1d8
Adam Smakulski <adam.smakulski@gmail.com>
2018-07-23 10:54:08 +0200

    validation of unnsupported separator. still in progress

--- src/FileContentReader.java
+++ src/FileContentReader.java
@@ -6,6 +6,8 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class FileContentReader {
     private String filePath;
@@ -55,23 +57,34 @@ public class FileContentReader {
     // metoda konwertujaca kolejne linie stringow do obiektow (parsowanie oraz formatowanie danych do wlasciwych typow)
     private ArrayList<Currency> convertIntoObject(ArrayList<String> read) {
         ArrayList<Currency> currencies = new ArrayList<>();
+
         for (String oneLine : read) {
-            String[] line = oneLine.split(",");
+            Pattern pattern = Pattern.compile(",\\d(\\d+|\\.\\d+)");
+            Matcher matcher = pattern.matcher(oneLine);
 
-            // formater, ktory konwertuje zrodlowa date yyyyMMdd do formatu DateTime yyyy-MM-dd
-            DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+            if (!matcher.matches()) {
+                System.out.println("Plik z danymi ma nieobslugiwane separatory. Jedyny obslugiwany separator to przecinek. \n" +
+                        "Wyjsc z programu, popraw i sprobuj ponownie");
+                MenuProject menu=new MenuProject();
+                menu.menuPanel();
+            }else {
+                String[] line = oneLine.split(",");
 
-            // parsowanie pol obiektow currency i wypelnenie nimi listy curriencies
-            Currency currency = new Currency(
-                    line[0],
-                    LocalDate.parse(line[1], dateTimeFormatter),
-                    Double.parseDouble(line[2]),
-                    Double.parseDouble(line[3]),
-                    Double.parseDouble(line[4]),
-                    Double.parseDouble(line[5]),
-                    Integer.parseInt(line[6])
-            );
-            currencies.add(currency);
+                // formater, ktory konwertuje zrodlowa date yyyyMMdd do formatu DateTime yyyy-MM-dd
+                DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+
+                // parsowanie pol obiektow currency i wypelnenie nimi listy curriencies
+                Currency currency = new Currency(
+                        line[0],
+                        LocalDate.parse(line[1], dateTimeFormatter),
+                        Double.parseDouble(line[2]),
+                        Double.parseDouble(line[3]),
+                        Double.parseDouble(line[4]),
+                        Double.parseDouble(line[5]),
+                        Integer.parseInt(line[6])
+                );
+                currencies.add(currency);
+            }
         }
         return currencies;
     }
