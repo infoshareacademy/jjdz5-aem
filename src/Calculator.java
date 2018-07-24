b0944b06aac4c9ff21938428c8cb764ac48ecdd1
Interial <intromalek@gmail.com>
2018-07-24 21:33:17 +0200

    Add files via upload

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.*;
 public class Calculator {
+
+
+//List<Currency> CurrentVariable.listCurrency = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
+
+    Integer value=Integer.parseInt(scanner.nextLine());
+
+    Currency expectedCurrency = new Currency();
+
+
+    public Integer getExpectedCurrencyVolume() {
+        return expectedCurrency.getVolume();
+    }
+    public String getExpectedCurrencyName(){return  expectedCurrency.getName();}
+public Calculator(){
+    Integer yourMultiplier = getExpectedCurrencyVolume()*value;
+    System.out.println("Po przeliczeniu ilość Waluty wynosi "+yourMultiplier+" "+getExpectedCurrencyName());}
+
+
 }
