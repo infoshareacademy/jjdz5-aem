7fc9a1e99d7851e745ce75cb79de3dfd1e1c728e
Interial <intromalek@gmail.com>
2018-07-27 19:33:30 +0200

    Jeszcze nie gotowe!
    brakuje dokończenia limitData

---
+++ src/LocalExtremum.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
+public class LocalExtremum
+{
+    public localExtremum()
+    {
+        local();
+    }
+
+
+    private SortCurrency sortCurrency = new SortCurrency();
+
+    private List<Currency> singleCurrency = new ArrayList<>();
+    public void local() {
+
+
+        System.out.println("Podaj walute");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String waluta = scanner.next();
+        FileContentReader fileContentReader = new FileContentReader();
+        fileContentReader.readFile();
+        for (Currency currency : fileContentReader.getListOfCurrencies()) {
+            if (currency.getName().equalsIgnoreCase(waluta)) {
+                singleCurrency.add(currency);
+            }
+        }
+        for (Currency currency : singleCurrency) {
+            System.out.println(currency);
+        }
+        sortSingleCurrency(singleCurrency);
+    }
+
+        private void sortSingleCurrency(List<Currency> list) {
+            Collections.sort(list,sortCurrency);
+        }
+
+    class SortCurrency implements Comparator<Currency> {
+        @Override
+        public int compare(Currency first, Currency second) {
+            return first.getClose().compareTo(second.getClose());
+
+            }
+    }
+    public limitData()
+    {
+        System.out.println("Podaj datę od której chcesz zacząć");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String rtDate = scanner.next();
+
+        System.out.println("Podaj datę na której chcesz skończyć");
+
+        Scanner scannr = new Scanner(System.in);
+
+        String dDate = scannr.next();
+
+        Integer startDate = parseInt(rtDate);
+        Integer endDate = parseInt(dDate);
+
+    }
+
+
+
+
+
+
+
+
+}
+
+
