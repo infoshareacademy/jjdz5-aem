f69bf2ed06d3e1fc2983e3ae083bb03badaaf4c2
Elżbieta Dolna <ela_dolna99@o2.pl>
2018-07-29 07:58:38 +0200

    delete local currency

--- src/MenuInformation.java
+++ src/MenuInformation.java
@@ -14,9 +14,7 @@ public class MenuInformation {
                 "2. Kalkulator walut\n"  +
                 "Umożliwia użytkownikowi przeliczenie kursu walut w określonej walucie i po określonym kursie. \n\n" +
                 "3 . Wyświetl ekstrema globalne \n" +
-                "Pokazuje użytkownikowi, kiedy wskazana waluta przyjmowała najwyższy oraz najniższy kurs \n\n" +
-                "4. Wyświetl ekstrema lokalne \n" +
-                "Pokazuje użytkownikowi, kiedy wskazana waluta przyjmowała najwyższy oraz najniższy kurs we wskazanym przez użytkownika zakresie czasowym\n\n" );
+                "Pokazuje użytkownikowi, kiedy wskazana waluta przyjmowała najwyższy oraz najniższy kurs \n\n" 
     }
 
     public void currentCurrency(){
@@ -31,9 +29,8 @@ public class MenuInformation {
         System.out.println("    1. Wyświetl dostępne  waluty i aktualny kurs na dzień wskazany przez użytkownika");
         System.out.println("    2. Kalkulator walut");
         System.out.println("    3. Wyświetl ekstrema globalne");
-        System.out.println("    4. Wyświetl ekstrema lokalne");
-        System.out.println("    5. Powrót do MENU");
-        System.out.println("    6. Informacja o programie");
+        System.out.println("    4. Powrót do MENU");
+        System.out.println("    5. Informacja o programie");
         System.out.println("    0. Zakończ działanie aplikacji");
     }
 
