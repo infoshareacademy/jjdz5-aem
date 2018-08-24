e0932401719e89f3a7bcc72e40e8f00779ebd8cd
AdamGlowicki <adamglowicki@wp.pl>
2018-08-24 14:14:14 +0200

    change "true" to other condition

--- src/main/java/com/infoshareacademy/aem/Main.java
+++ src/main/java/com/infoshareacademy/aem/Main.java
@@ -1,14 +1,10 @@
 package com.infoshareacademy.aem;
 
-import com.infoshareacademy.aem.global_extreme.GlobalExtreme;
-
 public class Main {
     public static void main(String[] args) {
 
         FileContentReader fileContentReader = new FileContentReader();
         MenuProject menu = new MenuProject();
-//        GlobalExtreme globalExtreme = new GlobalExtreme();
-//        globalExtreme.run();
 
         fileContentReader.readFile();
         menu.menuPanel();
