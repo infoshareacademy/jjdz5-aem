c37638fff7cc97ce5bdbe785bf40bac036b1ea71
Elka165 <ela_dolna99@o2.pl>
2018-09-01 18:25:42 +0200

    menu and footer

--- web-app/src/main/java/com/isa/aem/servlets/HelloServletAem.java
+++ web-app/src/main/java/com/isa/aem/servlets/HelloServletAem.java
@@ -1,5 +1,10 @@
 package com.isa.aem.servlets;
 
+import com.isa.aem.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,15 +12,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
 
 @WebServlet("/hello-servlet-aem")
 public class HelloServletAem extends HttpServlet {
 
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Override
+    public void init() throws ServletException {
+        System.out.println("servlet utworzony");
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html;charset=UTF-8");
-        PrintWriter writer = resp.getWriter();
-        writer.println("<!DOCTYPE html>");
-        writer.println("Hello World from AEM crew!");
+        Template template = templateProvider
+                .getTemplate(getServletContext(), "test");
+        try {
+            template.process(new HashMap<>(), resp.getWriter());
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
     }
 }
