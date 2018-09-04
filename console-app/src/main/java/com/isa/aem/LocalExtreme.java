1a1d423a2c7c05eb9f526577003959abb7555164
Adam Smakulski <adam.smakulski@gmail.com>
2018-09-05 00:09:01 +0200

    isCorrectFormat in progress

--- console-app/src/main/java/com/isa/aem/LocalExtreme.java
+++ console-app/src/main/java/com/isa/aem/LocalExtreme.java
@@ -1,8 +1,11 @@
 package com.isa.aem;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class LocalExtreme {
 
@@ -20,9 +23,14 @@ public class LocalExtreme {
     public LocalDate getDateFrom() {
         consolePrinter.printLocalExtremeWelcome();
         consolePrinter.printLn("Limit the date range for the currency of your choice. ");
-        while (isWithinRange(givenDate) == false) {
+        givenDate = LocalDate.parse(consoleReader.getString("Choose Date From: "));
+        while (isCorrectFormat(givenDate) == false) {
+            consolePrinter.printLn("The date you provide is incorrect format. Please use format yyyy-MM-dd:");
             givenDate = LocalDate.parse(consoleReader.getString("Choose Date From: "));
+        }
+        while (isWithinRange(givenDate) == false) {
             consolePrinter.printLn("The date you provide is out of range. Please try again:");
+            givenDate = LocalDate.parse(consoleReader.getString("Choose Date From: "));
         }
         return chosenDateFrom = givenDate;
     }
@@ -38,13 +46,40 @@ public class LocalExtreme {
 
 
     public boolean isWithinRange(LocalDate givenDate) {
-        for (Currency currency : CurrencyRepository.getCurrenciesSortedByDateAsc()) {
-            if (!currency.getDate().equals(givenDate)) {
-                return false;
-            }
-        }
+        if (givenDate.isBefore(CurrencyRepository.getCurrenciesSortedByDateAsc().get(0).getDate()) ||
+                givenDate.isAfter(CurrencyRepository.getCurrenciesSortedByDateAsc().get(CurrencyRepository.getCurrenciesSortedByDateAsc().size()-1).getDate())) {
+            return false;
+        } else
         return true;
     }
+
+    public boolean isCorrectFormat(LocalDate givenDate) {
+        Pattern pattern = Pattern.compile("([1-9]{1})([0-9]{3})\\-[0-2]{1}[1-2]{1}\\-[0-3]{1}[0-9]{1}");
+        Matcher matcher = pattern.matcher(givenDate.toString());
+        if (!matcher.matches()) {
+            return false;
+        }else {
+            return true;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     public List<String> getAvailableCurrencies() {
         for (Currency currency : CurrencyRepository.getCurrencies()) {
