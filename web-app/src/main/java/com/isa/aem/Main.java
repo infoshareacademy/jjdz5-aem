a62b230edc56307d935e221760783eadc327f5fd
Elka165 <ela_dolna99@o2.pl>
2018-12-04 21:31:37 +0100

    refactor

--- web-app/src/main/java/com/isa/aem/Main.java
+++ web-app/src/main/java/com/isa/aem/Main.java
@@ -1,21 +1,8 @@
 package com.isa.aem;
 
-import com.isa.aem.api.CurrencyReader;
-import com.isa.aem.api.HistoryLoaderNbp;
-import com.isa.aem.data_loaders.CurrencyNameCountryFlagsLoader;
-import com.isa.aem.data_loaders.FileContentReader;
-
 public class Main {
 
     public static void main(String[] args) {
-        HistoryLoaderNbp historyLoaderNbp=new HistoryLoaderNbp();
-        historyLoaderNbp.loadAllCurrencyHistoryFromNbpApi();
-        FileContentReader fileContentReader=new FileContentReader();
-
-        CurrencyReader currencyReader=new CurrencyReader();
-        fileContentReader.addPLNToListCurrency();
-        System.out.println(CurrencyRepository.getCurrencies());
-        System.out.println(CurrencyRepository.getCurrencies().get(0));
 
     }
 }
---
+++ web-app/src/main/java/com/isa/aem/api/ConversionApiOnCurrencyObject.java
@@ -0,0 +1,41 @@
+package com.isa.aem.api;
+
+import com.isa.aem.Currency;
+import com.isa.aem.CurrencyRepository;
+import com.isa.aem.api.nbp.CurrencyDetails;
+import com.isa.aem.api.nbp.CurrencyRates;
+import com.isa.aem.data_loaders.FileContentReader;
+import com.isa.aem.utils.DataValidator;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConversionApiOnCurrencyObject {
+    HistoryLoaderNbp historyLoaderNbp = new HistoryLoaderNbp();
+    public List<Currency> dateTable = new ArrayList<>();
+    DataValidator dataValidator = new DataValidator();
+
+    public void importCurrencyFromApiToTheStaticList() {
+        HistoryLoaderNbp historyLoaderNbp = new HistoryLoaderNbp();
+        historyLoaderNbp.loadAllCurrencyHistoryFromNbpApi();
+        parseApiTableOnCurrencyTable();
+        CurrencyRepository currencyRepository = new CurrencyRepository();
+        currencyRepository.setCurrencies(dateTable);
+        FileContentReader fileContentReader = new FileContentReader();
+        fileContentReader.addPLNToListCurrency();
+    }
+
+    public List<Currency> parseApiTableOnCurrencyTable() {
+        historyLoaderNbp.loadAllCurrencyHistoryFromNbpApi();
+        for (int iterator = 0; iterator < historyLoaderNbp.historyListNbp.size(); iterator++) {
+            for (CurrencyRates currencyRates : historyLoaderNbp.historyListNbp.get(iterator)) {
+                for (CurrencyDetails currencyDetails : currencyRates.getRates()) {
+                    LocalDate dateExchange = dataValidator.dataParse(currencyRates.getEffectiveDate().replace("-", ""));
+                    dateTable.add(new Currency(currencyDetails.getCode(), dateExchange, currencyDetails.getRate(), currencyDetails.getRate(), currencyDetails.getRate(), currencyDetails.getRate(), 0));
+                }
+            }
+        }
+        return dateTable;
+    }
+}
