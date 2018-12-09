2c834e41577e1933a4a9436a887456ca0374993c
Adam Smakulski <adam.smakulski@gmail.com>
2018-12-09 20:47:54 +0100

    simplified method getCurrenciesWithFullNameAndFlagWithoutPln()

--- currency-manager-lib/src/main/java/com/isa/aem/CurrencyRepository.java
+++ currency-manager-lib/src/main/java/com/isa/aem/CurrencyRepository.java
@@ -128,15 +128,9 @@ public class CurrencyRepository {
         return getSortedCurrencySet(currencyNameAndCountry);
     }
 
-    public List<Currency> getCurrenciesWithFullNameAndFlagWithoutPln() {
-        Set<Currency> currencyNameAndCountry = new HashSet<>();
-        List<Currency> currenciesFiltered = currencies.stream()
-                .filter(currency -> !currency.getName().equals("PLN"))
-                .collect(Collectors.toList());
-        for (Currency cc : currenciesFiltered) {
-            cc.setCurrencyNameCountryFlags(CurrencyNameCountryFlags.getCurrencies().get(cc.getName()));
-            currencyNameAndCountry.add(new Currency(cc.getName(), cc.getCurrencyNameCountryFlags()));
-        }
-        return getSortedCurrencySet(currencyNameAndCountry);
+    public Set<Currency> getCurrenciesWithFullNameAndFlagWithoutPln() {
+        return  getCurrenciesWithFullNameAndFlag().stream()
+                .filter(s -> !s.getName().equals("PLN"))
+                .collect(Collectors.toSet());
     }
 }
