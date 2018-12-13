15b15a81e977d2cd3085610a5fc9c534dac8c9de
AdamGlowicki <adamglowicki@wp.pl>
2018-12-13 18:24:33 +0100

    start activity raport

---
+++ web-app/src/main/java/com/isa/aem/servlets/ActivityRaport.java
@@ -0,0 +1,10 @@
+package com.isa.aem.servlets;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet
+public class ActivityRaport extends HttpServlet {
+
+
+}
